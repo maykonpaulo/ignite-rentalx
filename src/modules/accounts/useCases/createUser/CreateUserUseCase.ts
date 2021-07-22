d4fe9ab7d5a1889690a48fa57e80a99b1a0ac972
Maykon Paulo <maykons.df@gmail.com>
2021-07-22 08:41:43 -0300

    Adding account module

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcryptjs';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -10,17 +11,18 @@ class CreateUserUseCase {
         private usersRepository: IUsersRepository
     ) { }
 
-    async execute({ name, username, password, email, driver_license }: ICreateUserDTO): Promise<void> {
-        const userAlreadyExists = await this.usersRepository.findByName(name);
+    async execute({ name, password, email, driver_license }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
         if (userAlreadyExists) {
             throw new Error('User already exists');
         }
 
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
-            username,
-            password,
+            password: passwordHash,
             email,
             driver_license
         });
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,15 @@
 import { container } from 'tsyringe';
 
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository);
 
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository);
+
+container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
